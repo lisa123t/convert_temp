2bfdf834fe92de8938817d8b76d226c7d5de1f17
lisa123t <98433656+lisa123t@users.noreply.github.com>
2022-01-25 19:38:45 -0500

    Add files via upload

---
+++ convert_temps.js
@@ -0,0 +1,9 @@
+var f = prompt("Enter Farenheit temperature"); 
+
+    f = parseInt(f); 
+
+    let c = ((f - 32) * 5/9); 
+
+    c = parseInt(c); 
+    
+    alert("Farenheit temperature is " + f + "\n\nCelcius temperature is " + c); 
